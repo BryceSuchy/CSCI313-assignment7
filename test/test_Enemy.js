6b017869e11723ea71689de2fe4be33ac2828382
Bryce Suchy <bryceman1290@hotmail.com>
2018-03-26 19:20:43 -0500

    Added enemy class tests

--- test/test_Enemy.js
+++ test/test_Enemy.js
@@ -4,29 +4,70 @@ describe("Enemy", function () {
     let assert = chai.assert;
     let enemy;
 //Test fixture, create the same kind of player before each test
-    beforeEach(function() {
+    
 
     let game = sinon.stub();
         game.physics = sinon.stub();
         game.physics.enable = sinon.stub();
         let bullets = sinon.stub();
+	let frame = sinon.stub();
+	this.bounceTick = Math.random() * 2;
 
-        enemy = new Enemy(game, 0, 0, bullets);
 
-});
+        enemy = new Enemy(game, 0, 0, bullets, frame);
+
+
     it("Enemy can be created", function () {
         //We now build the enemy before each test case
         assert.isOk(true);
     });
 
-    it("Enemy can fire a shot", function () {
-        //enemy.fireShot();
+    it("can be updated without any keys being pressed", function () {
+        enemy.update();
         assert.isOk(true);
     });
 
-    it("can be updated without any keys being pressed", function () {
-        //enemy.update();
-        assert.isOk(true);
+    it("Enemy path 1 is valid with velocity change", function () {
+    	enemy.enemyPath = 1;
+	enemy.bouceTick = .02;
+	enemy.y = 0;
+	enemy.x = 350;
+	enemy.update();
+	assert.equal(enemy.body.velocity.x, -200);
+	assert.equal(enemy.y,  Math.sin(enemy.bounceTick) * 1);
+    });
+
+    it("Enemy path 1 is valid with reverse velocity change", function () {
+    	enemy.enemyPath = 1;
+	enemy.bouceTick = .02;
+	enemy.y = 0;
+	enemy.x = 1050;
+	enemy.update();
+	assert.equal(enemy.body.velocity.x, -175);
+	assert.equal(enemy.y,  Math.sin(enemy.bounceTick) * 1);
+    });
+
+
+    it("Enemy path 2 is valid", function () {
+    	enemy.enemyPath = 2;
+	enemy.bounceTick = .02;
+	enemy.y = Math.sin(enemy.bounceTick) * 1 + Math.cos(enemy.bounceTick) * 1;
+	enemy.update
+	assert.equal(enemy.y, 1.019798673359911);
+    });
+
+    it("Enemy path 3 is valid", function () {
+    	enemy.enemyPath = 3;
+	enemy.body.velocity.x = -200;
+	assert.equal(enemy.body.velocity.x, -200);
+    });
+
+    it("Enemy path 4 is valid", function () {
+    	enemy.enemyPath = 4;
+	enemy.bounceTick = .02;
+        enemy.y = 0;
+	enemy.update();
+	assert.equal(enemy.y, 0.03998933418663416);
     });
 
 
